0bcb99a631bf4a18044ea694adf33030322bc523
Ambika <ambikapuvvula@gmail.com>
2020-04-28 19:58:21 +0530

    Updates

---
+++ precache-manifest.f89bbca61c556bb1e475ee1e2970ddc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8783f4e1dba4c6ed2263641e45ca965",
+    "url": "/reactprofile/index.html"
+  },
+  {
+    "revision": "6a8e2e8db71c93160246",
+    "url": "/reactprofile/static/css/main.0fc39633.chunk.css"
+  },
+  {
+    "revision": "c41ac96aacb47c4afe4a",
+    "url": "/reactprofile/static/js/2.4696e22c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactprofile/static/js/2.4696e22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a8e2e8db71c93160246",
+    "url": "/reactprofile/static/js/main.c915b2f4.chunk.js"
+  },
+  {
+    "revision": "9f4a3aa8cae86a13aa04",
+    "url": "/reactprofile/static/js/runtime-main.10a81835.js"
+  }
+]);
